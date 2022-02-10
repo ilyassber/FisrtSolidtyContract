46c5bb7114ae959e0cdcfdbb5f7dd684811a5078
ilyassber <ilyass.berchida@gmail.com>
2022-02-10 23:32:27 +0100

    Making some code improvements

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -9,19 +9,16 @@ contract StorageFactory is SimpleStorage {
     SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
-        SimpleStorage simpleStorage = new SimpleStorage();
-        simpleStorageArray.push(simpleStorage);
+        simpleStorageArray.push(new SimpleStorage());
     }
 
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         // Address
         // ABI
-        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        simpleStorage.store(_simpleStorageNumber);
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        return simpleStorage.retrieve();
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
     }
 }
